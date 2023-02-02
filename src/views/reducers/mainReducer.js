3f394fa34a67cf1bd70553a9b45831ece3523fd0
misha25101991 <owen7@list.ru>
2023-02-02 12:44:17 +0300

    eslint edit reducers

--- src/views/reducers/mainReducer.js
+++ src/views/reducers/mainReducer.js
@@ -1,16 +1,19 @@
-import thunk from "redux-thunk"
+import thunk from 'redux-thunk'
 import { applyMiddleware } from 'redux'
 import { configureStore } from '@reduxjs/toolkit'
-import { combineReducers } from "redux"
+import { combineReducers } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import {searchReducer} from './searchReducer'
-import {getGoodsReducer} from './getGoodsReducer.js'
+import { searchReducer } from './searchReducer'
+import { getGoodsReducer } from './getGoodsReducer'
 
 const mainReducer = combineReducers({
-    search:searchReducer,
-    getGoods:getGoodsReducer
+  search: searchReducer,
+  getGoods: getGoodsReducer,
 })
 
-const store = configureStore({reducer:mainReducer},composeWithDevTools(applyMiddleware(thunk)))
+const store = configureStore(
+  { reducer: mainReducer },
+  composeWithDevTools(applyMiddleware(thunk)),
+)
 
 export default store
